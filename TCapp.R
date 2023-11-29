d9ae53933639fe5ba29714001470156ac5371ee0
Lyons <slyon@ad.unc.edu>
2023-11-29 16:03:32 -0500

    The app runs and changes with different options, but can't get it to work right for byos data.

--- TCapp.R
+++ TCapp.R
@@ -30,7 +30,8 @@ ui <- navbarPage("Table Converter",
                               textInput("expectedMasses", label = "Optional: Write in the expected masses, only separated by a comma:",
                                         value = ""),
                               br(),
-                              textInput("outputFileName", label = "Export file name:", value = "")
+                              textInput("outputFileName", label = "Export file name:", value = ""),
+                              downloadButton("downloadByos", "Download")
                               ),
                             conditionalPanel(
                               h3("Spectronaut"),
@@ -43,7 +44,8 @@ ui <- navbarPage("Table Converter",
                                         multiple = FALSE,
                                         accept = c(".xls")),
                               br(),
-                              textInput("outputFileName", label = "Export file name:", value = "")
+                              textInput("outputFileName", label = "Export file name:", value = ""),
+                              downloadButton("downloadSpectro", "Download")
                               ),
                             conditionalPanel(
                               h3("Perseus"),
@@ -56,8 +58,11 @@ ui <- navbarPage("Table Converter",
                                         multiple = FALSE,
                                         accept = c(".txt")),
                               br(),
-                              textInput("outputFileName", label = "Export file name:", value = "")
-                              )
+                              textInput("outputFileName", label = "Export file name:", value = ""),
+                              downloadButton("downloadPerseus", "Download")
+                              ),
+                            actionButton("convert", label = "Convert"),
+                            br()
                             ),
                             mainPanel(DT::dataTableOutput("dataframe"))
                           )))
@@ -68,19 +73,30 @@ server = function(input, output, session){
     
     req(input$byosFile)
     sheet_names = excel_sheets(input$byosFile) # extract all excel sheet names
-    file = lapply(sheet_names, function(x){
+    file1 = lapply(sheet_names, function(x){
       as.data.frame(read_excel(input$byosFile, sheet = x)) # read in each tab
     })
-    return(file)
+    return(file1)
     
   })
   
+  ByosSheetNames = reactive({
+    req(input$byosFile)
+    sheet_names = excel_sheets(input$byosFile)
+    return(sheet_names)
+  })
   
-  byosExpectedMasses = reactive({
+  byosExpectedFile = reactive({
     
     req(input$byosExpectedFile)
     file = read.xlsx(input$byosExpectedFile, sheet=1)
     return(file)
+    
+  })
+  
+  byosExpectedInput = reactive({
+    
+    expected = input$expectedMasses
     
   })
   
@@ -143,35 +159,19 @@ server = function(input, output, session){
   })
 
 
-  byos_expected = reactive({
+  run = eventReactive(input$convert,{
     
-    expected = input$expectedMasses
-    
-  })
-
-  
-  output$dataframe = DT::renderDataTable({
-    
-    if(input$SearchEngine == "Byos"){
+    DT::datatable({
       
-      byosConvert = function(byos_file, expected_masses_file, expected_masses_vector, writtenFileName){
+      if(input$SearchEngine == "Byos"){
         
-        if(is.null(expected_masses_file)){
-          expected = expected_masses_vector
-        }else{
-          expected = read_excel(expected_masses_file)[,2]
-          expected = unlist(expected)
-        }
+        expected = byosExpectedFile()
         
-        sheet_names = excel_sheets(byos_file) 
+        sheet_names = ByosSheetNames() 
         
-        byos_files = lapply(sheet_names, function(x){
-          as.data.frame(read_excel(byos_file, sheet = x))
-        })
+        byosdf = byosDf()
         
-        names(byos_files) = sheet_names
-        
-        nist = byos_files[[1]] %>%
+        nist = byosdf[[1]] %>%
           dplyr::select(., Name, Mass, `Expected mass`, Intensity) %>%
           mutate(Mass = as.numeric(Mass),
                  Intensity = as.numeric(Intensity)) %>%
@@ -186,7 +186,7 @@ server = function(input, output, session){
                         `Delta mass from expected`,
                         `Delta mass from most intense`, Intensity, `Local Rel. Int. (%)`)
         
-        samps = map2(expected, byos_files[2:length(byos_files)], function(x,y){
+        samps = map2(expected, byosdf[2:length(byosdf)], function(x,y){
           
           red2 = y %>%
             dplyr::select(., Name, Mass, `Expected mass`, Intensity) %>%
@@ -206,28 +206,89 @@ server = function(input, output, session){
                           `Delta mass from most intense`, Intensity, `Local Rel. Int. (%)`)
         })
         
-        hs = createStyle(textDecoration = "Bold", wrapText = TRUE)
+        lst = c(list(nist), samps)
         
-        write.xlsx(c(list(nist), samps), file = paste0(format(Sys.time(),'%Y%m%d_%H%M'), "_", writtenFileName, "_postprocessed.xlsx" ), 
-                   sheetName = sheet_names, overwrite = TRUE, headerStyle = hs)
         
+        
+        
+        # byosConvert = function(byos_files, expected_masses_file, expected_masses_vector){
+        #   
+        #   # if(is.null(expected_masses_file)){
+        #   #   expected = expected_masses_vector
+        #   # }else{
+        #   #   expected = expected_masses_file[,2]
+        #   #   expected = unlist(expected)
+        #   # }
+        #   
+        #   expected = expected_masses_file
+        #   
+        #   sheet_names = ByosSheetNames() 
+        #   
+        #   nist = byos_files[[1]] %>%
+        #     dplyr::select(., Name, Mass, `Expected mass`, Intensity) %>%
+        #     mutate(Mass = as.numeric(Mass),
+        #            Intensity = as.numeric(Intensity)) %>%
+        #     arrange(desc(Intensity)) %>%
+        #     mutate(`Expected mass` = as.numeric(`Expected mass`)) %>%
+        #     mutate("Delta mass from expected" = Mass - `Expected mass`) %>%
+        #     mutate("Delta mass from most intense" = dplyr::first(Mass) - Mass)  %>%
+        #     mutate("Local Rel. Int. (%)" = round(Intensity / dplyr::first(Intensity) * 100, 2), "%") %>%
+        #     dplyr::rename(., "Measured mass" = Mass) %>%
+        #     mutate(Intensity = formatC(Intensity, format = "e", digits = 2)) %>%
+        #     dplyr::select(., Name, `Measured mass`, `Expected mass`, 
+        #                   `Delta mass from expected`,
+        #                   `Delta mass from most intense`, Intensity, `Local Rel. Int. (%)`)
+        #   
+        #   samps = map2(expected, byos_files[2:length(byos_files)], function(x,y){
+        #     
+        #     red2 = y %>%
+        #       dplyr::select(., Name, Mass, `Expected mass`, Intensity) %>%
+        #       mutate(Mass = as.numeric(Mass),
+        #              Intensity = as.numeric(Intensity)) %>%
+        #       arrange(desc(Intensity)) %>%
+        #       mutate(`Expected mass` = as.numeric(`Expected mass`)) %>%
+        #       mutate("Delta mass from expected (ilab)" = Mass - x) %>%
+        #       mutate("Delta mass from expected (byos)" = Mass - `Expected mass`) %>%
+        #       mutate("Expected mass (ilab)" = rep(x, nrow(.))) %>%
+        #       mutate("Delta mass from most intense" = dplyr::first(Mass) - Mass)  %>%
+        #       mutate("Local Rel. Int. (%)" = round(Intensity / dplyr::first(Intensity) * 100, 2), "%") %>%
+        #       dplyr::rename(., "Measured mass" = Mass) %>%
+        #       mutate(Intensity = formatC(Intensity, format = "e", digits = 2)) %>%
+        #       dplyr::select(., Name, `Measured mass`, `Expected mass (ilab)`, `Delta mass from expected (ilab)`, 
+        #                     `Expected mass (byos)` = `Expected mass`, `Delta mass from expected (byos)`,
+        #                     `Delta mass from most intense`, Intensity, `Local Rel. Int. (%)`)
+        #   })
+        #   
+        #   lst = c(list(nist), samps)
+        #   
+        # }
+        # 
+        # byosConvert(byos_files = byosDf(),
+        #             expected_masses_file = byosExpectedFile(),
+        #             expected_masses_vector = byosExpectedInput())
         
       }
       
-      
-      byosConvert(byos_file = byosDf,
-                  expected_masses_file = byosExpectedMasses(),
-                  expected_masses_vector = byos_expected(),
-                  writtenFileName = input$outputFileName)
-      
-      
-    }
-    
+    })
     
   })
   
+  output$dataframe = DT::renderDataTable(run())
   
+  #Download excel spreadsheet
   
+  output$downloadByos = downloadHandler(
+    filename = function() {
+      paste0(format(Sys.time(),'%Y%m%d_%H%M'), "_", input$outputFileName, "_postprocessed.xlsx" )
+    },
+    content = function(file){
+      hs = createStyle(textDecoration = "Bold", wrapText = TRUE)
+      write.xlsx(output$dataframe, file, 
+                 sheetName = ByosSheetNames(), overwrite = TRUE, headerStyle = hs)
+      
+    }
+  )
+
 }
 
 shinyApp(ui, server)
