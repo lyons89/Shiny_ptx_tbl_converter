8490e7f1b756f01ea6e122896e333c97d9626b56
Scott Lyons <lyons0614@protonmail.com>
2026-09-08 14:35:33 -0400

    updated FP-Perseus to also recognize Welch and not just Student's

--- app.R
+++ app.R
@@ -45,7 +45,7 @@ APMS_FP = function(df){
                               "Combined Total Peptides")), 
                   contains("Difference"), contains("p-value"), contains("q-value", ignore.case = FALSE), contains("Significant"), 
                   ends_with("MaxLFQ Intensity"), ends_with("Spectral Count"), -any_of(c("Combined Unique Spectral Count", "Combined Spectral Count"))) %>%
-    dplyr::mutate(across(.cols = starts_with("LFQ intensity"), ~round(.x, 4)),
+    dplyr::mutate(across(.cols = contains("MaxLFQ Intensity"), ~round(.x, 4)),
                   across(.cols = contains("Difference"), ~round(.x, 4))) %>%
     dplyr::mutate("Summed LFQ Intensity" = round(rowSums(2^across(.cols = ends_with("MaxLFQ Intensity")), na.rm=TRUE)), 0) %>%
     dplyr::mutate(Contaminants = case_when(
@@ -60,9 +60,9 @@ APMS_FP = function(df){
                   contains("Difference"), contains("p-value"), contains("q-value", ignore.case = FALSE), contains("Significant"), 
                   ends_with("MaxLFQ Intensity"), ends_with("Spectral Count"),
                   -contains("significant", ignore.case=FALSE)) %>%
-    dplyr::rename_with(~gsub("^Student's T-test Difference", "Log2FC", .x), starts_with("Student's T-test Difference")) %>%
-    dplyr::rename_with(~gsub("^Student's T-test p-value", "p-value", .x), starts_with("Student's T-test p-value")) %>%
-    dplyr::rename_with(~gsub("^Student's T-test q-value", "q-value", .x), starts_with("Student's T-test q-value")) %>%
+    dplyr::rename_with(~gsub("^Student's|Welch's T-test Difference", "Log2FC", .x), matches("Student's|Welch's T-test Difference")) %>%
+    dplyr::rename_with(~gsub("^Student's|Welch's T-test p-value", "p-value", .x), matches("Student's|Welch's T-test p-value")) %>%
+    dplyr::rename_with(~gsub("^Student's|Welch's T-test q-value", "q-value", .x), matches("Student's|Welch's T-test q-value")) %>%
     dplyr::arrange(.,desc(`Summed LFQ Intensity`))
   
   
